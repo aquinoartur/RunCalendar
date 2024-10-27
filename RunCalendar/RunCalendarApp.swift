1aacd039487d923d010d1f1fd4fdecc7680092e6
Artur Aquino <artur-aquino1@hotmail.com>
2024-10-27 09:02:06 -0300

    Initial Commit

---
+++ RunCalendar/RunCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunCalendarApp.swift
+//  RunCalendar
+//
+//  Created by Adelmo Artur de  Aquino on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RunCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunCalendarTests/RunCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunCalendarTests.swift
+//  RunCalendarTests
+//
+//  Created by Adelmo Artur de  Aquino on 27/10/24.
+//
+
+import Testing
+@testable import RunCalendar
+
+struct RunCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
